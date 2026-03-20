0c8ef3ac59e9b501b0500a69cd95db20ea84e05e
andreasloewer <info@andreasloewer.de>
2026-03-20 10:54:29 +0100

    none

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,3 @@
 <?php
 
 defined('TYPO3') or die();
-
-call_user_func(function () {
-    $servicesFile = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName(
-        'EXT:al_backend_presets/Configuration/Services.yaml'
-    );
-
-    if (file_exists($servicesFile)) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['di']['services'][] = $servicesFile;
-    }
-});
